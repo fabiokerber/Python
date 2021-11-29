ee922819e8aef2b6b26e6c7391bbfc2b687b4fb6
Fabio <fabio.kerber@gmail.com>
2021-11-29 15:12:24 -0300

    Atualização de codigo

--- alura_01/03.py
+++ alura_01/03.py
@@ -5,7 +5,6 @@ print("#################################")
 numero_secreto = 42
 
 chute_str = input("Digite o seu numero: ") #RECEBE UM VALOR
-
 chute = int(chute_str) #CHUTE RECEBE O VALOR DE chute_str EM integer
 
 acertou = chute == numero_secreto
