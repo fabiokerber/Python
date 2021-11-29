d0b8866f8e01a642163989a42948e2c78a5a3702
Fabio <fabio.kerber@gmail.com>
2021-11-29 10:18:59 -0300

    Atualização de codigo

---
+++ alura_01/02.py
@@ -0,0 +1,18 @@
+print("#################################")
+print("Bem vindo ao jogo de Adivinhação!")
+print("#################################")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ") #RECEBE UM VALOR
+
+print("Você digitou: " , chute_str) #RECEBE O VALOR EM STRING. POR PADRAO SEMPRE ENTRA STRING
+
+chute = int(chute_str) #CHUTE RECEBE O VALOR DE chute_str EM integer
+
+if ( numero_secreto == chute ):
+    print("Você acertou!")
+else:
+    print("Você errou abestado!")
+
+print("Fim do jogo.")
