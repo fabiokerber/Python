89ba8d9373bb534123009ba65e030556c029f7cb
Fabio <fabio.kerber@gmail.com>
2021-12-01 07:48:50 -0300

    Atualização de codigo

---
+++ alura_01/07_format.py
@@ -0,0 +1,7 @@
+print("Tentativa {} de {}.".format(1,3))
+
+print("Tentativa {1} de {0}.".format(1,3))
+
+print("R$ {:.2f}".format(1.5)) # f = CONVERTE EM FLOAT
+
+print("R$ {:06.2f}".format(1758.5))
